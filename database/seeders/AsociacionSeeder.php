4a190214c77dcdd2e631f7665fedb63552341278
JoiSanchezMora <sanchezmorajoiner@gmail.com>
2023-10-20 17:42:54 -0500

    migraciones_1

---
+++ database/seeders/AsociacionSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AsociacionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
